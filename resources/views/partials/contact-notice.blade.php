39d132e896974a3c4565c48bf14d60e338937703
SamPratik <pratik.anwar@gmail.com>
2018-02-27 17:33:20 +0600

    contact-notice section added

---
+++ resources/views/partials/contact-notice.blade.php
@@ -0,0 +1,75 @@
+<section class="contact-notice">
+  <div class="container contact-notice-container">
+    <div class="row">
+      <div class="col-md-6 notice">
+        <h2>Notices</h2>
+        <div class="list-group notice-list">
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start notice-link">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">List group item heading</h5>
+              <small>3 days ago</small>
+            </div>
+            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+            <small>Donec id elit non mi porta.</small>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start notice-link">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">List group item heading</h5>
+              <small>3 days ago</small>
+            </div>
+            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+            <small>Donec id elit non mi porta.</small>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start notice-link">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">List group item heading</h5>
+              <small>3 days ago</small>
+            </div>
+            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+            <small>Donec id elit non mi porta.</small>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start notice-link">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">List group item heading</h5>
+              <small>3 days ago</small>
+            </div>
+            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+            <small>Donec id elit non mi porta.</small>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start notice-link">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">List group item heading</h5>
+              <small>3 days ago</small>
+            </div>
+            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+            <small>Donec id elit non mi porta.</small>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-6 contact">
+        <h2>Contact US</h2>
+        <div>
+          <p>Contact us and we'll get back to you within 24 hours.</p>
+          <p><strong><i class="fa fa-map-marker" aria-hidden="true"></i> Mirpur Cantonment, Dhaka</strong></p>
+          <p><strong><i class="fa fa-phone"></i> +880 1730-683164</strong></p>
+          <p><strong><i class="fa fa-envelope" aria-hidden="true"></i> lccmist@gmail.com</strong></p>
+        </div>
+        <form>
+          <div class="row">
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Your Name">
+            </div>
+            <div class="col">
+              <input type="email" class="form-control" placeholder="Your Email">
+            </div>
+          </div><br>
+          <div class="row">
+            <div class="col">
+              <textarea class="form-control" name="name" rows="8" cols="80" placeholder="Your Queries"></textarea>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
