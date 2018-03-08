70e29425a90cf94ebc8ab3a9ff2c4cb955453c28
SamPratik <pratik.anwar@gmail.com>
2018-03-09 00:02:56 +0600

    text color & background color has been added to tinymce

--- resources/views/admin/works/add.blade.php
+++ resources/views/admin/works/add.blade.php
@@ -133,39 +133,80 @@
 @push('scripts')
 	<script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
   <script>
-	  var editor_config = {
-	    path_absolute : "{{ URL::to('/') }}/",
-	    selector: "textarea#description",
-	    plugins: [
-	      "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-	      "searchreplace wordcount visualblocks visualchars code fullscreen",
-	      "insertdatetime media nonbreaking save table contextmenu directionality",
-	      "emoticons template paste textcolor colorpicker textpattern"
-	    ],
-	    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-	    relative_urls: false,
-	    file_browser_callback : function(field_name, url, type, win) {
-	      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
-	      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+		var editor_config = {
+			path_absolute : "{{ URL::to('/') }}/",
+			selector: "textarea#description",
+			plugins: [
+				"advlist autolink lists link image charmap print preview hr anchor pagebreak",
+				"searchreplace wordcount visualblocks visualchars code fullscreen",
+				"insertdatetime media nonbreaking save table contextmenu directionality",
+				"emoticons template paste textcolor colorpicker textpattern"
+			],
+			toolbar: "insertfile undo redo | styleselect | bold italic forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+			textcolor_map: [
+		    "000000", "Black",
+		    "993300", "Burnt orange",
+		    "333300", "Dark olive",
+		    "003300", "Dark green",
+		    "003366", "Dark azure",
+		    "000080", "Navy Blue",
+		    "333399", "Indigo",
+		    "333333", "Very dark gray",
+		    "800000", "Maroon",
+		    "FF6600", "Orange",
+		    "808000", "Olive",
+		    "008000", "Green",
+		    "008080", "Teal",
+		    "0000FF", "Blue",
+		    "666699", "Grayish blue",
+		    "808080", "Gray",
+		    "FF0000", "Red",
+		    "FF9900", "Amber",
+		    "99CC00", "Yellow green",
+		    "339966", "Sea green",
+		    "33CCCC", "Turquoise",
+		    "3366FF", "Royal blue",
+		    "800080", "Purple",
+		    "999999", "Medium gray",
+		    "FF00FF", "Magenta",
+		    "FFCC00", "Gold",
+		    "FFFF00", "Yellow",
+		    "00FF00", "Lime",
+		    "00FFFF", "Aqua",
+		    "00CCFF", "Sky blue",
+		    "993366", "Red violet",
+		    "FFFFFF", "White",
+		    "FF99CC", "Pink",
+		    "FFCC99", "Peach",
+		    "FFFF99", "Light yellow",
+		    "CCFFCC", "Pale green",
+		    "CCFFFF", "Pale cyan",
+		    "99CCFF", "Light sky blue",
+		    "CC99FF", "Plum"
+		  ],
+			relative_urls: false,
+			file_browser_callback : function(field_name, url, type, win) {
+				var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+				var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
 
-	      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-	      if (type == 'image') {
-	        cmsURL = cmsURL + "&type=Images";
-	      } else {
-	        cmsURL = cmsURL + "&type=Files";
-	      }
+				var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+				if (type == 'image') {
+					cmsURL = cmsURL + "&type=Images";
+				} else {
+					cmsURL = cmsURL + "&type=Files";
+				}
 
-	      tinyMCE.activeEditor.windowManager.open({
-	        file : cmsURL,
-	        title : 'Filemanager',
-	        width : x * 0.8,
-	        height : y * 0.8,
-	        resizable : "yes",
-	        close_previous : "no"
-	      });
-	    }
-	  };
+				tinyMCE.activeEditor.windowManager.open({
+					file : cmsURL,
+					title : 'Filemanager',
+					width : x * 0.8,
+					height : y * 0.8,
+					resizable : "yes",
+					close_previous : "no"
+				});
+			}
+		};
 
-	  tinymce.init(editor_config);
-	</script>
+		tinymce.init(editor_config);
+  </script>
 @endpush
