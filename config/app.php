fd9b5980b8d8dc8f75d90dea66691bb04554aa52
SamPratik <pratik.anwar@gmail.com>
2018-03-08 23:38:16 +0600

    added 'Laravel File Manager' package

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
 
         /*
          * Package Service Providers...
